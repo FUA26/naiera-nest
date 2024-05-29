5aa274fbbbc727c1eb41b1d0ea6aaecf52db885e
Fatih <fatihulil@gmail.com>
2024-05-29 19:06:17 +0700

    refactor: add swagger documatation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import appConfig from './configs/app.config';
+import appConfig from './configs/app/app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './configs/app/config.type';
+import { setupSwagger } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService) as ConfigService<AllConfigType>;
+
+  setupSwagger(app);
+
+  await app.listen(configService.get('app.port', { infer: true }));
 }
 bootstrap();
