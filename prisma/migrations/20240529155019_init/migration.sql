10a0c2841379f1d3fbb52e82174cdcdab3eeacf1
Fatih <fatihulil@gmail.com>
2024-05-29 23:26:30 +0700

    refactor: initial setup prisma orm

---
+++ prisma/migrations/20240529155019_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "hashToken" TEXT,
+    "role" "Role" NOT NULL,
+    "phoneNumber" TEXT,
+    "status" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
